cb17576167a643c6226f471cd5825c2d3b82c24e
VN Reddy Naini <vnreddynaini@VNs-MacBook-Air.local>
2022-12-12 06:05:28 +0530

    polymorphism 1

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,29 @@
 package com.driver;
 
 public class Main {
+    public static class Product {
+        public int product(int x, int y) {
+            return x;
+        }
 
+        public int product(int x, int y, int z) {
+            return x;
+        }
+
+        public double product(double x, double y) {
+            return x;
+        }
+    }
+
+    public static void main (String args[]){
+        Product p = new Product();
+        int x = 0;
+        int y = 0;
+        int z = 0;
+        double a = 0.0;
+        double b = 0.0;
+        p.product(x,y);
+        p.product(x,y,z);
+        p.product(a,b);
+    }
 }
